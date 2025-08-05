db07daea9248ae687095a7bc0abe6d86c32972ed
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-04 18:22:51 -0700

    The loader now throws an error if any of the data cannot be fetched successfully.

--- src/loaders.ts
+++ src/loaders.ts
@@ -1,14 +1,40 @@
 import { redirect } from 'react-router-dom';
 import { attemptTokenAuthentication } from './request/player_ms';
-import type { PlayerResponse } from './request/api_response';
+import type {
+  BalanceResponse,
+  BonusResponse,
+  PlayerResponse
+} from './request/api_response';
+import { attemptFetchPlayerBalance } from './request/currency_ms';
+import { attemptCheckDailyBonus } from './request/reward_ms';
 
-export async function homeLoader(): Promise<PlayerResponse> {
+export interface HomeInfo {
+  player: PlayerResponse;
+  balance: number;
+  bonus: BonusResponse;
+}
+
+export async function homeLoader(): Promise<HomeInfo> {
+  // Authenticate player token.
   const token = sessionStorage.getItem('token');
   if (!token) throw redirect('/welcome');
-  const response = await attemptTokenAuthentication(token);
-  if (response.status === 200) return response.body as PlayerResponse;
-  else {
-    sessionStorage.removeItem('token');
-    throw redirect('/welcome');
-  }
+
+  const promises = await Promise.all([
+    attemptTokenAuthentication(token),
+    attemptFetchPlayerBalance(token),
+    attemptCheckDailyBonus(token)
+  ]);
+
+  promises.forEach((response) => {
+    if (response.status !== 200) {
+      console.log(promises);
+      throw new Error('Could not pull player info from database.');
+    }
+  });
+
+  return {
+    player: promises[0].body as PlayerResponse,
+    balance: (promises[1].body as BalanceResponse).balance,
+    bonus: promises[2].body as BonusResponse
+  };
 }
