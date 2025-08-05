ec0298c136cdf8e28e3d70b6efd347337b234dc4
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-04 18:23:16 -0700

    Present all available player data to the player on the home page.

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,12 +1,16 @@
-import type { PlayerResponse } from '../request/api_response';
+import { type HomeInfo } from '../loaders';
 import { useLoaderData } from 'react-router-dom';
 
 export default function HomePage() {
-  const player = useLoaderData() as PlayerResponse;
+  const homeInfo = useLoaderData() as HomeInfo;
+  const { player, balance, bonus } = homeInfo;
 
   return (
     <>
-      <h1>Welcome, {player.username}.</h1>
+      <h1>Welcome, {player.username}</h1>
+      <h2>Current Balance: {balance}</h2>
+      <h3>Daily bonus is {bonus.available}</h3>
+      <h4>Current streak is {bonus.streak}</h4>
     </>
   );
 }
