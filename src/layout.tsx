dc753c98da4069732801e3fa6491bd900b4cb9eb
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-08 08:00:57 -0700

    - create two layouts: one with and one without a header.

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,6 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import Header from './header';
 
-export default function Layout() {
+export function LayoutWithHeader() {
+  return (
+    <div className='bg-background font-vt text-foreground'>
+      <Header />
+      <main className='min-h-screen'>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export function LayoutWithoutHeader() {
   return (
     <div className='bg-background font-vt text-foreground'>
       <main className='min-h-screen'>
