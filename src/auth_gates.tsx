a059810c9356d928ce621d83f28e45e4ad712115
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-08 07:26:10 -0700

    create AccountContext inside the RequireAuth component, so that all child components can easily access account data.

--- src/auth_gates.tsx
+++ src/auth_gates.tsx
@@ -1,11 +1,22 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, Outlet, useLoaderData, useLocation } from 'react-router-dom';
+import type { AccountInfo } from './util/loaders/auth';
+import { useState } from 'react';
+import { AccountContext } from './util/context';
 
 export function RequireAuth() {
+  const accountInfo = useLoaderData() as AccountInfo;
+  const [account, setAccount] = useState(accountInfo);
+
   const location = useLocation();
   const token = sessionStorage.getItem('token');
   if (!token)
     return <Navigate to='/welcome' replace state={{ from: location }} />;
-  return <Outlet />;
+
+  return (
+    <AccountContext.Provider value={{ account, setAccount }}>
+      <Outlet />
+    </AccountContext.Provider>
+  );
 }
 
 export function RequireNoAuth() {
