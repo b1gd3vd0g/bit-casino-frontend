a2754158325abceb8d7122fbf4c34df9864e351b
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-08 07:25:07 -0700

    Move the home loader into its own module inside util/loaders.

---
+++ src/util/loaders/auth.ts
@@ -0,0 +1,40 @@
+import { redirect } from 'react-router-dom';
+import { attemptTokenAuthentication } from '../request/player_ms';
+import type {
+  BalanceResponse,
+  BonusResponse,
+  PlayerResponse
+} from '../request/api_response';
+import { attemptFetchPlayerBalance } from '../request/currency_ms';
+import { attemptCheckDailyBonus } from '../request/reward_ms';
+
+export interface AccountInfo {
+  player: PlayerResponse;
+  balance: number;
+  bonus: BonusResponse;
+}
+
+export async function authLoader(): Promise<AccountInfo> {
+  // Authenticate player token.
+  const token = sessionStorage.getItem('token');
+  if (!token) throw redirect('/welcome');
+
+  const promises = await Promise.all([
+    attemptTokenAuthentication(token),
+    attemptFetchPlayerBalance(token),
+    attemptCheckDailyBonus(token)
+  ]);
+
+  promises.forEach((response) => {
+    if (response.status !== 200) {
+      console.log(promises);
+      throw new Error('Could not pull player info from database.');
+    }
+  });
+
+  return {
+    player: promises[0].body as PlayerResponse,
+    balance: (promises[1].body as BalanceResponse).balance,
+    bonus: promises[2].body as BonusResponse
+  };
+}
